491cbc9bedc7e12b5afb32fcac627a8273696454
taraskov97-tech <taras.kov.97@gmail.com>
2026-01-18 01:12:54 +0100

    Adding @builder dependency

--- src/main/java/ru/cors/springstudents/controller/StudentController.java
+++ src/main/java/ru/cors/springstudents/controller/StudentController.java
@@ -1,11 +1,22 @@
 package ru.cors.springstudents.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.cors.springstudents.model.Student;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
-    public List<Student>
+    @GetMapping
+    public List<Student> findAllStudent(){
+        return List.of(
+                Student.builder().firstName("Oleh").email("oleh12@gmail.com").age(41).build(),
+                Student.builder().firstName("Vasya").email("vasya12@gmail.com").age(41).build(),
+                Student.builder().firstName("Anna").email("anna12@gmail.com").age(41).build()
+                );
+    }
 }
--- src/main/java/ru/cors/springstudents/model/Student.java
+++ src/main/java/ru/cors/springstudents/model/Student.java
@@ -1,11 +1,18 @@
 package ru.cors.springstudents.model;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
+@Data
+@Builder
 public class Student {
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
+    @NonNull
     private String email;
+    private int age;
+
 
 }
